6a9322dfb1039673287ede35f150228c0678119d
Gabe <serenilgab@gmail.com>
2024-08-26 22:47:40 -0700

    so so so what

--- lab01/src/Arithmetic.java
+++ lab01/src/Arithmetic.java
@@ -17,7 +17,7 @@ public class Arithmetic {
     /** Computes sum of two ints (incorrectly).
      * @param a Value 1
      * @param b Value 2
-     * @return Sum of a and b if you want i guess i dont care
+     * @return Sum of a and b if you want i guess i dont know if this is working
      * */
     public static int sum(int a, int b) {
         return a + b;
